874c2a08595a348f216acb4799f677e1e024c570
pvm-mn <201248482+pvn-mn@users.noreply.github.com>
2026-06-08 20:48:59 +0530

    Task ID: P2-20 (Implement WebP images, lazy loading, srcset) - Initial Stage

--- patterns/about-collage.php
+++ patterns/about-collage.php
@@ -13,35 +13,46 @@ $right_image = get_field('about_collage_right_image', $page_id);
 ?>
 
 <section class="bg-[linear-gradient(to_bottom,#ffffff_0%,#ffffff_54%,#dff1ee_54%,#dff1ee_100%)] pt-12 pb-0">
-    <div class="relative mx-auto h-[600px] max-w-6xl px-6">
+<div class="relative mx-auto h-[600px] max-w-6xl px-6">
 
-        <?php if ($top_image) : ?>
-            <img
-                src="<?php echo esc_url($top_image['url']); ?>"
-                alt="<?php echo esc_attr($top_image['alt'] ?: 'About collage top image'); ?>"
-                class="absolute left-1/2 top-0 z-20 w-[500px] -translate-x-1/2 rounded-[28px] object-cover"
-                loading="lazy"
-            />
-        <?php endif; ?>
+    <?php 
+    if ( ! empty( $top_image['ID'] ) ) {
+        echo wp_get_attachment_image(
+            $top_image['ID'],
+            'large',
+            false,
+            [
+                'class' => 'absolute left-1/2 top-0 z-20 w-[500px] -translate-x-1/2 rounded-[28px] object-cover',
+                'alt'   => $top_image['alt'] ?: 'About collage top image',
+            ]
+        );
+    }
 
-        <?php if ($left_image) : ?>
-            <img
-                src="<?php echo esc_url($left_image['url']); ?>"
-                alt="<?php echo esc_attr($left_image['alt'] ?: 'About collage left image'); ?>"
-                class="absolute left-0 top-[145px] z-30 w-[460px] rounded-[28px] object-cover"
-                loading="lazy"
-            />
-        <?php endif; ?>
+    if ( ! empty( $left_image['ID'] ) ) {
+        echo wp_get_attachment_image(
+            $left_image['ID'],
+            'large',
+            false,
+            [
+                'class' => 'absolute left-0 top-[145px] z-30 w-[460px] rounded-[28px] object-cover',
+                'alt'   => $left_image['alt'] ?: 'About collage left image',
+            ]
+        );
+    }
 
-        <?php if ($right_image) : ?>
-            <img
-                src="<?php echo esc_url($right_image['url']); ?>"
-                alt="<?php echo esc_attr($right_image['alt'] ?: 'About collage right image'); ?>"
-                class="absolute right-0 top-[265px] z-10 w-[460px] rounded-[28px] object-cover"
-                loading="lazy"
-            />
-        <?php endif; ?>
+    if ( ! empty( $right_image['ID'] ) ) {
+        echo wp_get_attachment_image(
+            $right_image['ID'],
+            'large',
+            false,
+            [
+                'class' => 'absolute right-0 top-[265px] z-10 w-[460px] rounded-[28px] object-cover',
+                'alt'   => $right_image['alt'] ?: 'About collage right image',
+            ]
+        );
+    }
+    ?>
 
-    </div>
+</div>
 
 </section>
